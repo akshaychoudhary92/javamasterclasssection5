5365abfdd00033a5caaac8caf496aa93a84ca98b
Akshay Choudhary <ak92choudhary@gmail.com>
2019-11-13 18:10:18 -0500

    commiting

---
+++ sectionFive/src/main/java/com/company/SwitchChallenge.java
@@ -0,0 +1,58 @@
+package com.company;
+
+public class SwitchChallenge {
+    public static void main(String[] args) {
+//        char switchValue = 'A';
+//
+//        switch(switchValue){
+//            case 'A':
+//                System.out.println("its A");
+//                break;
+//            case 'B':
+//                System.out.println("its B");
+//                break;
+//            case 'C':
+//                System.out.println("its C");
+//                break;
+//            case 'D':
+//                System.out.println("its D");
+//                break;
+//                default:
+//                    System.out.println("enter a char dumbass");
+//        }
+
+//        int findValue = 0;
+//        int counter = 0;
+//
+//        for(int i = 1; i <= 1000; i++){
+//            if((i % 3 == 0) && (i % 5 == 0)){
+//                System.out.println(i);
+//                findValue += i;
+//                counter++;
+//
+//
+//            }else if(counter == 5){
+//                System.out.println(findValue);
+//                break;
+//            }
+//
+//        }
+//                isOdd(5);
+
+    }
+//    public static boolean isOdd(int number){
+//        if(number < 0){
+//            System.out.println("false");
+//            return false;
+//        }
+//        if(number % 2 == 1){
+//            System.out.println("true");
+//            return true;
+//        }
+//
+//        return false;
+//    }
+
+    
+
+}
